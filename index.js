da954b7b2261793e05869ffcfb851bf428588199
César Higashi <76760288+higashi04@users.noreply.github.com>
2021-03-05 21:21:50 -0600

    finished CRUD directory

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const path = require('path');
 const Puestos = require('./models/puestos')
 
@@ -14,20 +15,51 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log('Database Connected, bro')
-})
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
-app.get('/makepuestos', async (req, res) => {
-    const puesto = new Puestos({title: 'tacos 5 x 20'});
-    await puesto.save();
-    res.send(puesto);
+app.get('/puestos', async (req, res) =>{
+    const puestos = await Puestos.find({});
+    res.render('puestos/index', { puestos })
+});
 
-})
+
+app.post('/puestos', async (req, res) =>{
+   const puesto = new Puestos(req.body);
+   await puesto.save();
+   res.redirect(`/puestos/${puesto._id}`)
+});
+
+app.get('/puestos/new', (req, res) =>{
+    res.render('puestos/nuevo.ejs');
+});
+app.get('/puestos/:id', async (req, res) =>{
+    const puesto = await Puestos.findById(req.params.id);   
+    res.render('puestos/show', { puesto });
+});
+
+app.get('/puestos/:id/edit', async(req, res) =>{
+    const puesto = await Puestos.findById(req.params.id);
+     res.render('puestos/edit', { puesto });
+});
+
+app.put('/puestos/:id', async(req, res) =>{
+    const { id } = req.params;
+    const puesto = await Puestos.findByIdAndUpdate(id, {...req.body})
+    res.redirect(`/puestos/${puesto._id}`);
+});
+
+app.delete('/puestos/:id', async(req, res) => {
+    const { id } = req.params;
+    await Puestos.findByIdAndDelete(id);
+    res.redirect('/puestos')
+} )
 
 app.listen(3000, ()=> {
     console.log("they listening to us!! on port three thousand minus seven!!!!")
