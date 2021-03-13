193860387158f3f7f4540464802f1b6085f852ec
César Higashi <76760288+higashi04@users.noreply.github.com>
2021-03-13 12:58:50 -0600

    added some Express error handling

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const engine = require('ejs-mate')
+const engine = require('ejs-mate');
+const JOI = require('joi');
 const path = require('path');
-const Puestos = require('./models/puestos')
+const Puestos = require('./models/puestos');
+const AppError = require('./AppError');
+const AsyncErrors = require('./AsyncErrors');
 
 mongoose.connect('mongodb://localhost:27017/puestosdetacos', {
     useNewUrlParser: true,
@@ -26,42 +29,66 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 
-app.get('/puestos', async (req, res) =>{
+app.get('/puestos', AsyncErrors(async(req, res) =>{
     const puestos = await Puestos.find({});
     res.render('puestos/index', { puestos })
-});
+}));
 
 
-app.post('/puestos', async (req, res) =>{
-   const puesto = new Puestos(req.body);
-   await puesto.save();
-   res.redirect(`/puestos/${puesto._id}`)
-});
+app.post('/puestos', AsyncErrors(async (req, res, next) =>{
+        const PuestosSchema = JOI.object({
+            Puesto: JOI.object({
+                title: JOI.string().required(),
+                calle: JOI.string().required(),
+            }).required()
+        })
+        const { error } = PuestosSchema.validate(req.body);
+        if (error){
+            const msg = error.details.map(el => el.message).join(',')
+            throw new AppError(msg, 400)
+        }
+        console.log(result);
+        const puesto = new Puestos(req.body.Puesto);
+        await puesto.save();
+        res.redirect(`/puestos/${puesto._id}`)    
+})); 
 
 app.get('/puestos/new', (req, res) =>{
     res.render('puestos/nuevo.ejs');
 });
-app.get('/puestos/:id', async (req, res) =>{
+app.get('/puestos/:id', AsyncErrors(async (req, res) =>{
     const puesto = await Puestos.findById(req.params.id);   
     res.render('puestos/show', { puesto });
-});
+}));
 
-app.get('/puestos/:id/edit', async(req, res) =>{
+app.get('/puestos/:id/edit', AsyncErrors(async(req, res) =>{
     const puesto = await Puestos.findById(req.params.id);
      res.render('puestos/edit', { puesto });
-});
+}));
 
-app.put('/puestos/:id', async(req, res) =>{
+app.put('/puestos/:id', AsyncErrors(async(req, res) =>{
     const { id } = req.params;
-    const puesto = await Puestos.findByIdAndUpdate(id, {...req.body})
+    const puesto = await Puestos.findByIdAndUpdate(id, {...req.body.Puesto})
     res.redirect(`/puestos/${puesto._id}`);
-});
+}));
 
-app.delete('/puestos/:id', async(req, res) => {
+app.delete('/puestos/:id', AsyncErrors(async(req, res) => {
     const { id } = req.params;
     await Puestos.findByIdAndDelete(id);
     res.redirect('/puestos')
-} )
+}));
+
+app.all('*', (req, res, next) => {
+    next(new AppError('Sitio No Existe', 404))
+});
+
+app.use((err, req, res, next) => {
+    const { status = 500} = err;
+    if (!err.message) err.message = 'vos la habeis cajeteado'
+    res.status(status).render('error', { err })
+   
+});
+
 
 app.listen(3000, ()=> {
     console.log("they listening to us!! on port three thousand minus seven!!!!")
